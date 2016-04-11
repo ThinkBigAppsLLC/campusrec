1ae40e20b96cbd9b49cdc29a709072cd07f6890b
Evan Lee <el12b@my.fsu.edu>
2016-04-11 19:52:25 -0400

    Contact Information Fragment set up. Contact Adapter set up. Now retrieves information from the website itself.

---
+++ app/src/main/java/edu/fsu/campusrec/ContactAdapter.java
@@ -0,0 +1,130 @@
+package edu.fsu.campusrec;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHolder>{
+
+    private ArrayList<Contact> contactList;
+    private Context context;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView name_label;
+        TextView title_label;
+        ImageView action_phone;
+        ImageView action_email;
+
+        public ViewHolder (View v){
+            super(v);
+            name_label = (TextView) v.findViewById(R.id.label_name);
+            title_label = (TextView) v.findViewById(R.id.label_title);
+            action_phone = (ImageView) v.findViewById(R.id.action_call);
+            action_email = (ImageView) v.findViewById(R.id.action_email);
+        }
+    }
+
+    public static class Contact {
+        private String name;
+        private String title;
+        private String phone;
+        private String email;
+
+        public Contact (@NonNull String name, @NonNull String title, @NonNull String phone, @NonNull String email){
+            this.name = name;
+            this.title = title;
+            this.phone = phone;
+            this.email = email;
+        }
+
+        public String getName(){
+            return this.name;
+        }
+
+        public String getTitle(){
+            return this.title;
+        }
+
+        public String getPhone(){
+            return "tel:"+this.phone;
+        }
+
+        public String getEmail(){
+            return this.email;
+        }
+    }
+
+    public ContactAdapter(@NonNull ArrayList<Contact> contacts, Context context) {
+        this.contactList = contacts;
+        this.context = context;
+    }
+
+    @Override
+    public ContactAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View item = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.list_contact, parent, false);
+        RelativeLayout container = (RelativeLayout) item.findViewById(R.id.contact_container);
+        switch(viewType){
+            case 0:
+                container.setBackgroundColor(ContextCompat.getColor(context, R.color.colorBackground));
+                break;
+            case 1:
+                container.setBackgroundColor(ContextCompat.getColor(context, R.color.colorSecondaryBackground));
+                break;
+        }
+        return new ViewHolder(item);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int pos) {
+        final Contact person = contactList.get(pos);
+        holder.name_label.setText(person.getName());
+        holder.title_label.setText(person.getTitle());
+
+        holder.action_phone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse(person.getPhone()));
+                context.startActivity(intent);
+            }
+        });
+
+        holder.action_email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
+                emailIntent.setType("plain/text");
+                emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{person.getEmail()});
+
+                context.startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            }
+        });
+    }
+
+    @Override
+    public int getItemViewType(int position){
+        return position % 2;
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+}
--- app/src/main/java/edu/fsu/campusrec/ContactFragment.java
+++ app/src/main/java/edu/fsu/campusrec/ContactFragment.java
@@ -1,25 +1,109 @@
 package edu.fsu.campusrec;
 
-
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Html;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class ContactFragment extends Fragment {
+    private static final String CONTACT_URL = "http://campusrec.fsu.edu/staff/contact-our-team";
 
-    public ContactFragment() {
+    public ContactFragment() { }
+    private ArrayList<ArrayList<ContactAdapter.Contact>> contacts;
+
+    public class CustomLinearLayoutManager extends LinearLayoutManager {
+        public CustomLinearLayoutManager(Context context) {
+            super(context);
+        }
+        @Override
+        public boolean canScrollVertically() {
+            return false;
+        }
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //createData();
+        GetDetails task = new GetDetails();
+        task.execute(CONTACT_URL);
         View v = inflater.inflate(R.layout.fragment_contact, container, false);
+        RecyclerView contactRecycler = (RecyclerView) v.findViewById(R.id.list_crao);
+        CustomLinearLayoutManager mRecyclerManager = new CustomLinearLayoutManager(getContext());
+        ContactAdapter contactAdapter;
+        try {
+            task.get();
+            contactAdapter = new ContactAdapter(contacts.get(0), getContext());
+            contactRecycler.setAdapter(contactAdapter);
+            contactRecycler.setLayoutManager(mRecyclerManager);
+            contactRecycler.setHasFixedSize(true);
+        } catch (InterruptedException|ExecutionException e) {
+            e.printStackTrace();
+        }
+
 
         return v;
+    }
+
+    private class GetDetails extends AsyncTask<String, Void, ArrayList<ArrayList<ContactAdapter.Contact>>> {
+        @Override
+        protected ArrayList<ArrayList<ContactAdapter.Contact>> doInBackground(String... urls) {
+            ArrayList<ArrayList<ContactAdapter.Contact>> tmpContacts = new ArrayList<>();
+            ArrayList<ContactAdapter.Contact> tmp;
+
+            try {
+                Document doc  = Jsoup.connect(CONTACT_URL).get();
+                Elements tables = doc.select("thead");
+                for(Element table : tables){
+                    tmp = new ArrayList<>();
+                    ContactAdapter.Contact tmpContact = null;
+                    String name;
+                    String title;
+                    String phone;
+                    String email;
+
+                    Element body = table.nextElementSibling();
+                    Elements rows = body.children();
+                    rows.remove(0);
+                    for(Element row : rows){
+                        Elements fields = row.children().select("td");
+                        name = fields.get(0).text();
+                        title = fields.get(1).text();
+                        phone = fields.get(2).text();
+                        email = fields.get(3).text();
+                        tmpContact = new ContactAdapter.Contact(name, title, phone, email);
+                        tmp.add(tmpContact);
+                    }
+                    tmpContacts.add(tmp);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            contacts = tmpContacts;
+            return tmpContacts;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<ArrayList<ContactAdapter.Contact>> result) { }
     }
 
 }
